ad6de6b32c95b222f47f6651a2ac1ac2e2820960
YooHyukLim <dbgur3065@gmail.com>
2014-05-15 02:12:34 +0900

    Map

--- src/standard/Map_piece.java
+++ src/standard/Map_piece.java
@@ -1,5 +1,17 @@
 package standard;
 
 public class Map_piece {
-
+	int current;
+	int post_map;
+	int next_map;
+	int map_number;
+	
+	public Map_piece(){
+	}
+	
+	public Map_piece(int current, int post, int next){
+		this.current = current;
+		this.post_map = post;
+		this.next_map = next;
+	}
 }
