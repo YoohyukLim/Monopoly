0715614e7ef267c24fab00071efecaf4e120c54e
YooHyukLim <dbgur3065@gmail.com>
2014-05-21 01:22:07 +0900

    Map 클래스 분리

--- src/standard/Monopoly.java
+++ src/standard/Monopoly.java
@@ -5,71 +5,24 @@ import java.util.Random;
 
 public class Monopoly {
 	static int Dice_num = 6;
-	static int Map_number_length = 20;
-	static int Map_size=36;
-	static int Map_side = 4;
-	static Map_piece [] Map_pieces;
+
+	static int piece_num = 4;
+	static Piece [] player = new Piece[piece_num];
 	
 	public static void main(String args[]){
 		Map map = new Map();
 		map.generate_map();
 		
-		for(int i=0; i<Map_size; i++){
-			System.out.print(Map_pieces[i].map_number+" ");
-			if((i+1) % (Map_size/Map_side) == 0)
+		for(int i=0; i<piece_num; i++){
+			player[i] = new Piece(i, 0);
+		}
+		
+		for(int i=0; i<Map.Map_size; i++){
+			System.out.print(Map.Map_pieces[i].map_number+" ");
+			if((i+1) % (Map.Map_size/Map.Map_side) == 0)
 				System.out.print("\n");
 		}
 		System.out.print("\n");
-	}
-	
-	static class Map{
-		public Map(){
-		}
-		
-		public void generate_map(){
-			Map_pieces = new Map_piece[Map_size];
-			int special_map = 0;
-			
-			Calendar now = Calendar.getInstance();
-			int seed = now.get(Calendar.MILLISECOND);
-			Random r = new Random();
-			r.setSeed(seed);
-			
-			Map_pieces[0] = new Map_piece(0, Map_size-1, 1);
-			Map_pieces[0].map_number = special_map++;
-			for(int i=1; i<Map_size-1; i++){
-				Map_pieces[i] = new Map_piece(i, i-1, i+1);
-				if((i+1) % (Map_size/Map_side) == 1)
-					Map_pieces[i].map_number = special_map++;
-				else
-					Map_pieces[i].map_number = Math.abs(r.nextInt() % (Map_number_length-3)) + 3;
-			}
-			Map_pieces[Map_size-1] = new Map_piece(Map_size-1, Map_size-2, 0);
-			Map_pieces[Map_size-1].map_number = Math.abs(r.nextInt() % (Map_number_length-3)) + 3;
-		}
-		
-		public void generate_map(int size){
-			Map_size = size;
-			Map_pieces = new Map_piece[Map_size];
-			int special_map = 0;
-			
-			Calendar now = Calendar.getInstance();
-			int seed = now.get(Calendar.MILLISECOND);
-			Random r = new Random();
-			r.setSeed(seed);
-			
-			Map_pieces[0] = new Map_piece(0, Map_size-1, 1);
-			Map_pieces[0].map_number = special_map++;
-			for(int i=1; i<Map_size-1; i++){
-				Map_pieces[i] = new Map_piece(i, i-1, i+1);
-				if((i+1) % (Map_size/Map_side) == 1)
-					Map_pieces[i].map_number = special_map++;
-				else
-					Map_pieces[i].map_number = Math.abs(r.nextInt() % (Map_number_length-3)) + 3;
-			}
-			Map_pieces[Map_size-1] = new Map_piece(Map_size-1, Map_size-2, 0);
-			Map_pieces[Map_size-1].map_number = Math.abs(r.nextInt() % (Map_number_length-3)) + 3;
-		}
 	}
 	
 	static class Dice{
