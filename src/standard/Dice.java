906f81d641c074eed4259cb1793513f39384135c
hoy9090 <hoy9090@naver.com>
2014-05-26 21:30:16 +0900

    Revert "hello"
    This reverts commit cd2e0d475103bcd912f7282575c283312ee37389.

--- src/standard/Dice.java
+++ src/standard/Dice.java
@@ -7,7 +7,6 @@ public class Dice {
 	static int Dice_num = 6;
 	
 	public Dice(){
-		System.out.println("hello");
 	}
 	
 	public int exec(){
@@ -15,6 +14,7 @@ public class Dice {
 		int seed = now.get(Calendar.MILLISECOND);
 		Random r = new Random();
 		r.setSeed(seed);
+		
 		return Math.abs(r.nextInt() % Dice_num);
 	}
 }
