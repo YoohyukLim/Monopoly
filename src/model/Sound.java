1741a4a779a11977f8bb9ee0a4c6c0735df78003
YooHyukLim <dbgur3065@gmail.com>
2014-06-11 03:18:10 +0900

    sound 모델 추가

---
+++ src/model/Sound.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sound {
+	File filepath;
+	   AudioInputStream audioIn;
+	   Clip clip;
+	   
+	   public Sound(String filename) throws Exception{
+		   filepath = new File(filename);
+		   audioIn = AudioSystem.getAudioInputStream(filepath);
+		   clip = AudioSystem.getClip();
+		   clip.open(audioIn);
+	   }
+	   
+	   public void play(){
+		   clip.start();
+	   }
+	   
+	   public void loop(){
+		   clip.loop(clip.LOOP_CONTINUOUSLY);
+	   }
+	   
+	   public void stop(){
+		   clip.stop();
+	   }
+	   
+	   public boolean isplaying(){
+		   return clip.isRunning();
+	   }
+}
