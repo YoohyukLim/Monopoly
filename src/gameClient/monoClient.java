c9d5081ceb35e63d67c5b251e2eddc4e5da98537
YooHyukLim <dbgur3065@gmail.com>
2014-06-23 02:17:45 +0900

    소켓 ㅆㅃ

--- src/gameClient/monoClient.java
+++ src/gameClient/monoClient.java
@@ -5,24 +5,24 @@ import gui.NicknameSet;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
 import protocol.ChatProtocol;
 import protocol.GameProtocol;
 import protocol.LoginProtocol;
 import protocol.Protocol;
 
-public class monoClient {
+public class monoClient extends Thread{
 	public static String name;
 	String serverIp = "127.0.0.1";
 
 	static Socket socket;
-	public static ObjectInputStream in;
-	public static ObjectOutputStream out;
+	static ObjectOutputStream out;
+	static ObjectInputStream in;
 	static Protocol data;
 
 	public static JFrame m_frame;
@@ -42,97 +42,47 @@ public class monoClient {
 			System.out.println("서버에 연결 중 IP: " + serverIp);
 			socket = new Socket(serverIp, 7777);
 
-			/*out = new ObjectOutputStream(socket.getOutputStream());
-			in = new ObjectInputStream(socket.getInputStream());*/
-
 			System.out.println("서버에 연결되었습니다.");
-
-			Thread sender = new Thread(new ClientSender(name));
-			Thread receiver = new Thread(new ClientReceiver());
-			sender.start();
-			receiver.start();
-
+		
+			out = new ObjectOutputStream(socket.getOutputStream());
+			in = new ObjectInputStream(socket.getInputStream());
+			
+			out.writeObject(new LoginProtocol(name, LoginProtocol.ENTER));
+			
 			lobby = new Lobby(this);
+			
+			this.start();
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.exit(0);
 		}
+	}
+	
+	public void run(){
+		while(in!=null){
+			try {
+				data = (Protocol) in.readObject();
+				
+				if (data instanceof ChatProtocol)
+					analysisChatProtocol((ChatProtocol) data);
+				else if (data instanceof GameProtocol)
+					analysisGameProtocol((GameProtocol) data);
+			} catch (ClassNotFoundException e) {
+				System.out.println("Class Not Found");
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void analysisChatProtocol(ChatProtocol data){
+	}
+	
+	public void analysisGameProtocol(GameProtocol data){
 	}
 
 	public void exit() {
 		System.out.println("exit");
-		try {
-			socket.close();
-			System.exit(0);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		System.exit(0);
 	}
-
-	static class ClientSender extends Thread {
-		ObjectOutputStream out;
-		String name;
-
-		ClientSender(String name) {
-			this.name = name;
-
-			try {
-				out = new ObjectOutputStream(socket.getOutputStream());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}// 생성자
-
-		public void run() {
-			try {
-				out.writeObject(new LoginProtocol(name, LoginProtocol.LOGIN_CHECK));
-
-				while (out != null) {
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}// run()
-	}// ClientSender()
-
-	static class ClientReceiver extends Thread {
-		ObjectInputStream in;
-
-		ClientReceiver() {
-			try {
-				in = new ObjectInputStream(socket.getInputStream());
-			} catch (IOException e) {
-			}
-		}// 생성자
-
-		public void run() {
-
-			try {
-				while (in != null) {
-					data = (Protocol) in.readObject();
-
-					if (data instanceof ChatProtocol)
-						analysisChatProtocol((ChatProtocol) data);
-					else if (data instanceof GameProtocol)
-						analysisGameProtocol((GameProtocol) data);
-				}
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					socket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		public void analysisChatProtocol(ChatProtocol data) {
-		}
-
-		public void analysisGameProtocol(GameProtocol data) {
-		}
-	}// ClientReceiver()
 }
--- src/gameServer/monoServer.java
+++ src/gameServer/monoServer.java
@@ -10,93 +10,110 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 
+import protocol.ChatProtocol;
+import protocol.GameProtocol;
 import protocol.LoginProtocol;
 import protocol.Protocol;
 
 public class monoServer {
 	HashMap<String, ObjectOutputStream> clients;
 	Protocol data;
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		new monoServer().start();
 	}
-	
-	public monoServer(){
+
+	public monoServer() {
 		clients = new HashMap<String, ObjectOutputStream>();
 		Collections.synchronizedMap(clients);
 	}
-	
-	public void start(){
+
+	public void start() {
 		ServerSocket serverSocket = null;
 		Socket socket = null;
-		
+
 		try {
 			serverSocket = new ServerSocket(7777);
 			System.out.println("서버가 준비되었습니다.");
-			
-			while(true){
+
+			while (true) {
 				System.out.println("연결요청을 기다립니다.");
 				socket = serverSocket.accept();
-				
-				System.out.println("["+socket.getInetAddress()+":"+socket.getPort()+"]"+"에서 접속하였습니다.");
+
+				System.out.println("[" + socket.getInetAddress() + ":"
+						+ socket.getPort() + "]" + "에서 접속하였습니다.");
 				new ServerReceiver(socket).start();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}//start()
-	
-	class ServerReceiver extends Thread{
+	}// start()
+
+	class ServerReceiver extends Thread {
 		Socket socket;
 		ObjectInputStream in;
 		ObjectOutputStream out;
 		String name;
 
-		ServerReceiver(Socket socket){
+		ServerReceiver(Socket socket) {
 			this.socket = socket;
-			
+
 			try {
-				in = new ObjectInputStream (socket.getInputStream());
-				out = new ObjectOutputStream (socket.getOutputStream());
-			} catch (Exception e) {}
+				in = new ObjectInputStream(socket.getInputStream());
+				out = new ObjectOutputStream(socket.getOutputStream());
+			} catch (Exception e) {
+			}
 		}
-		
-		public void run(){
-			try{
+
+		public void run() {
+			try {
 				data = (Protocol) in.readObject();
 				name = data.getName();
-				
-				if(data instanceof LoginProtocol)
+
+				if (data instanceof LoginProtocol)
 					clients.put(name, out);
 				else
 					System.exit(0);
-				
-				System.out.println(name+"님이 접속하셨습니다.");
-				System.out.println("현재 접속자 수는 "+clients.size()+"입니다.");
-				
-				while(in!=null){
-					//sendToAll(in.readUTF());
+
+				System.out.println(name + "님이 접속하셨습니다.");
+				System.out.println("현재 접속자 수는 " + clients.size() + "입니다.");
+
+				while (in != null) {
+					data = (Protocol) in.readObject();
+
+					if (data instanceof ChatProtocol)
+						analysisChatProtocol((ChatProtocol) data);
+					else if (data instanceof GameProtocol)
+						analysisGameProtocol((GameProtocol) data);
 				}
-			} catch(IOException e){
+			} catch (IOException e) {
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} finally {
 				clients.remove(name);
-				System.out.println("["+socket.getInetAddress()+":"+socket.getPort()+"]"+"에서 접속을 종료하였습니다.");
-				System.out.println("현재 접속자 수는 "+clients.size()+"입니다.");
+				System.out.println("[" + socket.getInetAddress() + ":"
+						+ socket.getPort() + "]" + "에서 접속을 종료하였습니다.");
+				System.out.println("현재 접속자 수는 " + clients.size() + "입니다.");
 			}
-		}//run()
-	}//ServerReceiver()
-	
-	void sendToAll(String msg){
-		Iterator it = clients.keySet().iterator();
+		}// run()
 
-		while(it.hasNext()){
-			try {
-				ObjectOutputStream out = new ObjectOutputStream((OutputStream) clients.get(it.next()));
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
+		private void analysisGameProtocol(GameProtocol data) {
 		}
-	}//sendToAll
+
+		private void analysisChatProtocol(ChatProtocol data) {
+		}
+
+		void sendToAll(String msg) {
+			Iterator it = clients.keySet().iterator();
+
+			while (it.hasNext()) {
+				try {
+					ObjectOutputStream out = new ObjectOutputStream(
+							(OutputStream) clients.get(it.next()));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}// sendToAll
+	}// ServerReceiver()
 }
