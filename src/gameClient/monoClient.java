12535e7ec9cc12ff637ff69fa733be83113d893b
YooHyukLim <dbgur3065@gmail.com>
2014-06-22 17:14:13 +0900

    서버 및 클라이언트 틀 생성

--- src/gameClient/monoClient.java
+++ src/gameClient/monoClient.java
@@ -1,5 +1,66 @@
 package gameClient;
 
-public class monoClient {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
+public class monoClient {
+	public static void main(String args[]){
+		try {
+			String serverIp = "127.0.0.1";
+			System.out.println("서버에 연결 중 IP: "+serverIp);
+			Socket socket = new Socket(serverIp, 7777);
+			
+			System.out.println("서버에 연결되었습니다.");
+			
+			Thread sender = new Thread(new ClientSender(socket, args[0]));
+			Thread receiver = new Thread(new ClientReceiver(socket));
+			
+			sender.start();
+			receiver.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}//main()
+	
+	static class ClientSender extends Thread{
+		Socket socket;
+		ObjectOutputStream out;
+		String name;
+		
+		ClientSender(Socket socket, String name){
+			this.socket = socket;
+			
+			try {
+				out = new ObjectOutputStream(socket.getOutputStream());
+				this.name = name;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}//생성자
+		
+		public void run(){		
+			while(out!=null){
+			}
+		}//run()
+	}//ClientSender()
+	
+	static class ClientReceiver extends Thread{
+		Socket socket;
+		ObjectInputStream in;
+		
+		ClientReceiver(Socket socket){
+			this.socket = socket;
+			
+			try{
+				in = new ObjectInputStream(socket.getInputStream());
+			} catch (IOException e){}
+		}//생성자
+		
+		public void run(){
+			while(in!=null){
+			}
+		}//run()
+	}//ClientReceiver()
 }
--- src/gameServer/monoServer.java
+++ src/gameServer/monoServer.java
@@ -1,7 +1,14 @@
 package gameServer;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import standard.Map;
 
@@ -11,5 +18,75 @@ public class monoServer {
 	
 	public monoServer(){
 		clients = new HashMap<String, ObjectOutputStream>();
+		Collections.synchronizedMap(clients);
+	}
+	
+	public void start(){
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		
+		try {
+			serverSocket = new ServerSocket(7777);
+			System.out.println("서버가 준비되었습니다.");
+			
+			while(true){
+				System.out.println("연결요청을 기다립니다.");
+				socket = serverSocket.accept();
+				
+				System.out.println("["+socket.getInetAddress()+":"+socket.getPort()+"]"+"에서 접속하였습니다.");
+				new ServerReceiver(socket).start();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}//start()
+	
+	class ServerReceiver extends Thread{
+		Socket socket;
+		ObjectInputStream in;
+		ObjectOutputStream out;
+
+		ServerReceiver(Socket socket){
+			this.socket = socket;
+			
+			try {
+				in = new ObjectInputStream (socket.getInputStream());
+				out = new ObjectOutputStream (socket.getOutputStream());
+			} catch (Exception e) {}
+		}
+		
+		public void run(){
+			try{
+				/*name = in.readUTF();
+				
+				clients.put(name, out);
+				System.out.println("현재 접속자 수는 "+clients.size()+"입니다.");*/
+				while(in!=null){
+					sendToAll(in.readUTF());
+				}
+			} catch(IOException e){
+			} finally {
+				//clients.remove(name);
+				
+				System.out.println("["+socket.getInetAddress()+":"+socket.getPort()+"]"+"에서 접속을 종료하였습니다.");
+				System.out.println("현재 접속자 수는 "+clients.size()+"입니다.");
+			}
+		}//run()
+	}//ServerReceiver()
+	
+	void sendToAll(String msg){
+		Iterator it = clients.keySet().iterator();
+
+		while(it.hasNext()){
+			try {
+				ObjectOutputStream out = new ObjectOutputStream((OutputStream) clients.get(it.next()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+		}
+	}//sendToAll
+	
+	public static void main(String args[]){
+		new monoServer().start();
 	}
 }
