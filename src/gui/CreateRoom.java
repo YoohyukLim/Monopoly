8435ece72a570b933b15b5155a098ad387aa2b11
SangkyuLee <hoy9090@naver.com>
2014-06-22 23:18:28 +0900

    JFrame으로 고침

--- src/gui/CreateRoom.java
+++ src/gui/CreateRoom.java
@@ -9,36 +9,37 @@ public class CreateRoom extends JPanel {
 	private JButton createButton;
 	private JButton cancelButton;
 	private JLabel createRoomLabel = new JLabel("방 제목을 입력하세요!");
+	private Container cp;
 	public static JFrame f;
 	
 	public CreateRoom() {
+		f = new JFrame();
+		f.setSize(380,150);
+		f.setResizable(false);
+		f.setVisible(true);
+		cp = f.getContentPane();
+		
 		roomId = new JTextField(10);
 		createButton = new JButton();
 		cancelButton = new JButton();
 		createRoomLabel.setFont(new Font("굴림", Font.BOLD, 15));
-		this.add(roomId);
-		this.add(createButton);
-		this.add(cancelButton);
-		this.add(createRoomLabel);
-		createRoomLabel.setBounds(130, 25, 180, 25);
-		roomId.setBounds(70,75,130,25);
-		createButton.setBounds(220,75,70,25);
-		cancelButton.setBounds(320,75,70,25);
+		cp.add(roomId);
+		cp.add(createButton);
+		cp.add(cancelButton);
+		cp.add(createRoomLabel);
+		createRoomLabel.setBounds(100, 25, 180, 25);
+		roomId.setBounds(40,75,130,25);
+		createButton.setBounds(190,75,70,25);
+		cancelButton.setBounds(280,75,70,25);
 		createButton.setText("확인");
 		cancelButton.setText("취소");
 		
-		this.setLayout(null);
-		this.setVisible(true);
-
+		cp.setLayout(null);
 	}
 
 
 	public static void main(String[] args) {
-		f = new JFrame();
-		f.setSize(405,150);
-		f.setResizable(false);
-		f.getContentPane().add(new CreateRoom());
-		f.setVisible(true);
+		new CreateRoom();
 	}
 
 }
--- src/gui/Lobby.java
+++ src/gui/Lobby.java
@@ -1,46 +1,46 @@
 package gui;
 
+import java.awt.Container;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
-import javax.swing.JPanel;
 
 @SuppressWarnings({ "rawtypes", "serial" })
-public class Lobby extends JPanel {
+public class Lobby extends JFrame {
 	
 	private JList roomList;
 	private JList userList;
 	private JButton roomCreateButton;
 	private String[] room = {"room1", "room2", "room3"};
 	private String[] user = {"user1", "user2", "user3"};
+	private Container cp;
 	public static JFrame f;
 	
 	@SuppressWarnings("unchecked")
 	public Lobby() {
+		f = new JFrame();
+		f.setSize(330, 340);
+		f.setResizable(false);
+		f.setVisible(true);
+		cp = f.getContentPane();
 		roomList = new JList(room);
 		userList = new JList(user);
 		roomCreateButton = new JButton();
 		roomCreateButton.setText("방 만들기");
 		
-		this.add(roomList);
-		this.add(userList);
-		this.add(roomCreateButton);
+		cp.setLayout(null);
+		cp.add(roomList);
+		cp.add(userList);
+		cp.add(roomCreateButton);
 		
 		roomList.setBounds(30, 30, 140, 240);
 		userList.setBounds(200, 30, 100, 140);
 		roomCreateButton.setBounds(200, 200, 100, 70);
-		
-		this.setLayout(null);
-		this.setVisible(true);
-
 	}
 	
 	public static void main(String[] args) {
-		f = new JFrame();
-		f.setSize(330, 340);
-		f.setResizable(false);
-		f.getContentPane().add(new Lobby());
-		f.setVisible(true);
+		new Lobby();
 	}
 
 }
