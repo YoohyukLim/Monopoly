5ff4e3760ee2dffb02b8a2c85efada76b789a8e0
YooHyukLim <dbgur3065@gmail.com>
2014-06-22 20:43:31 +0900

    Chat/Login DATA

--- src/protocol/ChatProtocol.java
+++ src/protocol/ChatProtocol.java
@@ -2,6 +2,24 @@ package protocol;
 
 public class ChatProtocol implements Protocol{
 	private static final long serialVersionUID = 1L;
+	
+	private static final short ENTER = 1000;
+	private static final short LOGIN_CHECK = 1100;
+	public static final short LOGIN_ERROR = 1200;
+	public static final short MESSAGE = 2000;
+	public static final short MESSAGE_SLIP = 2100;
+	public static final short EXIT = 3000;
+	public static final short SEND_USER_LIST = 4000;
+	public static final short SEND_TOTAL_USER = 4100;
+	
+	private String name, message;
+	private short state;
+	
+	public ChatProtocol(String name, String message, short state){
+		this.name = name;
+		this.message = message;
+		this.state = state;
+	}
 
 	@Override
 	public short getProtocol() {
@@ -10,19 +28,25 @@ public class ChatProtocol implements Protocol{
 
 	@Override
 	public String getName() {
-		return null;
+		return name;
 	}
 
 	@Override
 	public String getMessage() {
-		return null;
+		return message;
+	}
+	
+	public short state(){
+		return state;
 	}
 
 	@Override
 	public void setMessage(String message) {
+		this.message = message;
 	}
 
 	@Override
 	public void setName(String name) {
+		this.name = name;
 	}
 }
--- src/protocol/LoginProtocol.java
+++ src/protocol/LoginProtocol.java
@@ -2,6 +2,23 @@ package protocol;
 
 public class LoginProtocol implements Protocol {
 	private static final long serialVersionUID = 1L;
+	
+	private static final short ENTER = 1000;
+	private static final short LOGIN_CHECK = 1100;
+	public static final short LOGIN_ERROR = 1200;
+	public static final short MESSAGE = 2000;
+	public static final short MESSAGE_SLIP = 2100;
+	public static final short EXIT = 3000;
+	public static final short SEND_USER_LIST = 4000;
+	public static final short SEND_TOTAL_USER = 4100;
+	
+	private String name;
+	private short state;
+	
+	public LoginProtocol(String name, short state){
+		this.name = name;
+		this.state = state;
+	}
 
 	public short getProtocol() {
 		return 0;
@@ -9,7 +26,7 @@ public class LoginProtocol implements Protocol {
 
 	@Override
 	public String getName() {
-		return null;
+		return name;
 	}
 
 	@Override
@@ -23,5 +40,6 @@ public class LoginProtocol implements Protocol {
 
 	@Override
 	public void setName(String name) {
+		this.name = name;
 	}
 }
