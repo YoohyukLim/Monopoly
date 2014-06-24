0acb5d84c2c7cebd91f8c9e78f38084ebfec6b30
YooHyukLim <dbgur3065@gmail.com>
2014-06-25 06:13:48 +0900

    lobby

--- src/gui/Lobby.java
+++ src/gui/Lobby.java
@@ -22,7 +22,7 @@ import protocol.LobbyProtocol;
 
 @SuppressWarnings({ "rawtypes", "serial" })
 public class Lobby extends JFrame {
-	
+
 	private JList roomList;
 	private JList userList;
 	private JButton roomCreateButton;
@@ -31,11 +31,11 @@ public class Lobby extends JFrame {
 	private Container cp;
 	private monoClient monoClient;
 	public static JFrame f;
-	
+
 	@SuppressWarnings("unchecked")
 	public Lobby(monoClient monoClient) {
 		this.monoClient = monoClient;
-		
+
 		f = new JFrame();
 		f.setSize(330, 340);
 		f.setResizable(false);
@@ -44,54 +44,59 @@ public class Lobby extends JFrame {
 		cp = f.getContentPane();
 		rooms = new DefaultListModel<>();
 		clients = new DefaultListModel<>();
-		roomList = new JList(new DefaultListModel());
+		JScrollPane roomPane = new JScrollPane();
+		roomList = new JList(new DefaultListModel<>());
 		roomList.addMouseListener(new roomHandler());
-		
-		userList = new JList(new DefaultListModel());
+		roomPane.setViewportView(roomList);
+
+		JScrollPane userPane = new JScrollPane();
+		userList = new JList(new DefaultListModel<>());
 		roomCreateButton = new JButton();
 		roomCreateButton.setText("방 만들기");
 		roomCreateButton.addActionListener(new makeRoomHandler());
-		
+		userPane.setViewportView(userList);
+
 		cp.setLayout(null);
-		cp.add(roomList);
-		cp.add(userList);
+		cp.add(roomPane);
+		cp.add(userPane);
 		cp.add(roomCreateButton);
-		
-		roomList.setBounds(30, 30, 140, 240);
-		userList.setBounds(200, 30, 100, 140);
+
+		roomPane.setBounds(30, 30, 140, 240);
+		userPane.setBounds(200, 30, 100, 140);
 		roomCreateButton.setBounds(200, 200, 100, 70);
 	}
-	
-	public void refreshClients(ArrayList<String> clients){
+
+	public void refreshClients(ArrayList<String> clients) {
 		int length = clients.size();
 		this.clients = (DefaultListModel) userList.getModel();
 		this.clients.removeAllElements();
-		for(int i=0; i<length; i++){
-			if(monoClient.name.equals(clients.get(i)))
-				this.clients.addElement(clients.get(i)+"<나>");
+		for (int i = 0; i < length; i++) {
+			if (monoClient.name.equals(clients.get(i)))
+				this.clients.addElement(clients.get(i) + "<나>");
 			else
 				this.clients.addElement(clients.get(i));
 		}
 	}
-	
-	public void refreshRooms(ArrayList<String> rooms){
+
+	public void refreshRooms(ArrayList<String> rooms) {
 		int length = rooms.size();
 		this.rooms = (DefaultListModel) roomList.getModel();
 		this.rooms.removeAllElements();
-		for(int i=0; i<length; i++)
+		for (int i = 0; i < length; i++)
 			this.rooms.addElement(rooms.get(i));
 	}
-	
-	class roomHandler implements MouseListener{
+
+	class roomHandler implements MouseListener {
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			JList list = (JList) e.getSource();
-			
-			if(e.getClickCount() == 2){
+
+			if (e.getClickCount() == 2) {
 				int num = list.locationToIndex(e.getPoint());
 				String RoomName = list.getModel().getElementAt(num).toString();
-				LobbyProtocol data = new LobbyProtocol(monoClient.name, LobbyProtocol.ENTER_ROOM);
+				LobbyProtocol data = new LobbyProtocol(monoClient.name,
+						LobbyProtocol.ENTER_ROOM);
 				data.setRoomName(RoomName);
 				monoClient.sendToServer(data);
 			}
@@ -113,22 +118,22 @@ public class Lobby extends JFrame {
 		public void mouseExited(MouseEvent e) {
 		}
 	}
-	
-	class makeRoomHandler implements ActionListener{
+
+	class makeRoomHandler implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			new CreateRoom(monoClient);
 		}
 	}
-	
-	class windowHandler extends WindowAdapter{
-		public void windowClosing(WindowEvent e){
+
+	class windowHandler extends WindowAdapter {
+		public void windowClosing(WindowEvent e) {
 			e.getWindow().setVisible(false);
 			e.getWindow().dispose();
 			monoClient.exit();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		new Lobby(null);
 	}
